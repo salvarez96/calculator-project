702a79d4aae0abc960f872ff2f696e6d3e8ee8e7
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2022-09-22 01:43:52 -0500

    Finish number printing logic
    Now the calculator accepts integers with no more than 17 digits and also would only show one - for negative numbers and only accepts one . to create floating numbers. It also doesn´t allow infinite number printing, including printing many zeroes. The number shown in the calculator is the exact number that will be used to create the operations logic

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const calculations = document.getElementById("operations-box--calculations");
+const calculations = document.querySelector(".operations-box--calculations");
 
 const arrKeyCode = [];
 const arrKeyNameCode = [];
@@ -65,117 +65,152 @@ let results = [];
 let total = [];
 let bigTotal;
 
-const getKey = (keyPress) => {
-    const keyCode = keyPress.keyCode;
-    const keyNameCode = keyPress.code;
-    const keyName = keyPress.key;
-    arrKeyCode.push(keyCode);
-    arrKeyNameCode.push(keyName);
-    // console.log(arrKeyCode, arrKeyNameCode, keyNameCode);
-
-    // if(saveAll.length == 0){
-    //     calculations.innerText = 0;
-    // }
-
-    const operation = (numberKey) => {
-        if(Number.isFinite(numberKey)){
-            count += String(numberKey);
-            console.log(count);
-            numbers.push(Number(count));
-            numbers.shift();
-            console.log(numbers);
-        }
-        if (keyCode == numPadOperationKeyCode["+"]){
-            results.push(numbers[0]);
-            console.log(results);
-            count = 0;
-            if(results.length > 1){
-                total.push(numbers[0] + results[results.length - 1]);
-                console.log(total);
-                console.log(total[total.length - 1]);
-            }
-            // bigTotal = total.map(item => item + results[results.length - 1]);
-            // console.log(bigTotal);
-            // // let result = numbers.reduce((sum, element) => sum + element, numbers[numbers.length-1]);
-            // // console.log(result);
-            // const operate = results[results.length - 1] + Number(count);
-            // console.log(operate);
-            // // results.push(result);
-            // // numbers.push(operate);
-            // console.log(numbers);
-            // // numbers.shift();
-        }
-        if (keyCode == numPadOperationKeyCode["-"]){
-            numbers.push(Number(count));
-            // let result = numbers.reduce((sum, element) => sum - element, 0);
-            const operate = numbers.shift() - Number(count);
-            // results.push(result);
-            numbers.push(operate);
-            console.log(numbers);
-            console.log(results);
-            console.log(operate);
-            count = 0;
-            numbers.shift();
-        }
-        if (keyCode == numPadOperationKeyCode["*"]){
-            numbers.push(Number(count));
-            const operate = numbers.shift() * Number(count);
-            // results.push(result);
-            numbers.push(operate);
-            console.log(numbers);
-            console.log(results);
-            console.log(operate);
-            count = 0;
-            numbers.shift();
-        }
-        if (keyCode == numPadOperationKeyCode["/"]){
-            numbers.push(Number(count));
-            const operate = numbers.shift() / Number(count);
-            // results.push(result);
-            numbers.push(operate);
-            console.log(numbers);
-            console.log(results);
-            console.log(operate);
-            count = 0;
-            numbers.shift();
-        }
-
-        // if(keyName === "+" || keyName === "-" || keyName === "*" || keyName === "/"){
-        //     numbers.push(count);
-        //     count = 0;
-        //     console.log(numbers);
-        //     let operate = Number(numbers[numbers.length - 2] + keyPress.keyCode + numbers[numbers.length - 1]);
-        //     console.log(operate);
-        //     console.log(typeof(operate));
-        //     console.log(numberKey);
-        // }
-        if (keyName === "Enter"){
-            numbers = [0];
-            results = [];
-        }
+function writeNumber(numberKey){
+    let keyCode = numberKey.keyCode;
+    
+    if(keyCode === 189 || keyCode === 109 && calculations.innerText === '0'){
+        calculations.innerText = '-'
     }
 
-    operation(Number(keyName));
-
-    // if(keyName === "Backspace"){
-    //     saveAll.pop();
-    //     count --;
-    //     calculations.innerText -= saveAll[saveAll.length - 1];
-    //     // saveAll.forEach((value) => {
-
-    //     // })
-    //     return console.log(calculations.innerText, saveAll, count);
-    // } else {
-    //     saveAll.push(keyName);
-    //     count ++
-    //     calculations.innerText += saveAll[saveAll.length - 1];
-    //     // saveAll.forEach((value) => {
-
-    //     // })
-    //     return console.log(calculations.innerText, saveAll, count);
-    // }
-
-
-    // console.log(keyPress)
+    if(keyCode === 110 && calculations.innerText === '0'){
+        calculations.innerText = '0.'
+    }
+    
+    if(calculations.innerText[0] === '-' && keyCode === 110 && !calculations.innerText.includes('-0.')){
+        calculations.innerText = '-0.'
+    }
+    
+    if(    keyCode > 95 && keyCode < 106 
+        || keyCode > 47 && keyCode < 59 
+        || !calculations.innerText.includes('.') && keyCode === 110
+        ){
+        if(calculations.innerText.length < 18){
+            const numberInput = Number(numberKey.key);
+            console.log(numberInput);
+            if(calculations.innerText === '-0.' || calculations.innerText === '0.' || calculations.innerText != 0){
+                calculations.innerText += numberKey.key;
+                count = Number(calculations.innerText);
+            } else {
+                calculations.innerText = numberKey.key;
+            }
+        }
+    }
+    console.log(count)
 }
-const keys = document.addEventListener("keydown", getKey)
+
+document.addEventListener('keydown', writeNumber);
+
+// const getKey = (keyPress) => {
+//     const keyCode = keyPress.keyCode;
+//     const keyNameCode = keyPress.code;
+//     const keyName = keyPress.key;
+//     arrKeyCode.push(keyCode);
+//     arrKeyNameCode.push(keyName);
+//     // console.log(arrKeyCode, arrKeyNameCode, keyNameCode);
+
+//     // if(saveAll.length == 0){
+//     //     calculations.innerText = 0;
+//     // }
+
+//     const operation = (numberKey) => {
+//         if(Number.isFinite(numberKey)){
+//             count += String(numberKey);
+//             console.log(count);
+//             numbers.push(Number(count));
+//             numbers.shift();
+//             console.log(numbers);
+//         }
+//         if (keyCode == numPadOperationKeyCode["+"]){
+//             results.push(numbers[0]);
+//             console.log(results);
+//             count = 0;
+//             if(results.length > 1){
+//                 total.push(numbers[0] + results[results.length - 1]);
+//                 console.log(total);
+//                 console.log(total[total.length - 1]);
+//             }
+//             // bigTotal = total.map(item => item + results[results.length - 1]);
+//             // console.log(bigTotal);
+//             // // let result = numbers.reduce((sum, element) => sum + element, numbers[numbers.length-1]);
+//             // // console.log(result);
+//             // const operate = results[results.length - 1] + Number(count);
+//             // console.log(operate);
+//             // // results.push(result);
+//             // // numbers.push(operate);
+//             // console.log(numbers);
+//             // // numbers.shift();
+//         }
+//         if (keyCode == numPadOperationKeyCode["-"]){
+//             numbers.push(Number(count));
+//             // let result = numbers.reduce((sum, element) => sum - element, 0);
+//             const operate = numbers.shift() - Number(count);
+//             // results.push(result);
+//             numbers.push(operate);
+//             console.log(numbers);
+//             console.log(results);
+//             console.log(operate);
+//             count = 0;
+//             numbers.shift();
+//         }
+//         if (keyCode == numPadOperationKeyCode["*"]){
+//             numbers.push(Number(count));
+//             const operate = numbers.shift() * Number(count);
+//             // results.push(result);
+//             numbers.push(operate);
+//             console.log(numbers);
+//             console.log(results);
+//             console.log(operate);
+//             count = 0;
+//             numbers.shift();
+//         }
+//         if (keyCode == numPadOperationKeyCode["/"]){
+//             numbers.push(Number(count));
+//             const operate = numbers.shift() / Number(count);
+//             // results.push(result);
+//             numbers.push(operate);
+//             console.log(numbers);
+//             console.log(results);
+//             console.log(operate);
+//             count = 0;
+//             numbers.shift();
+//         }
+
+//         // if(keyName === "+" || keyName === "-" || keyName === "*" || keyName === "/"){
+//         //     numbers.push(count);
+//         //     count = 0;
+//         //     console.log(numbers);
+//         //     let operate = Number(numbers[numbers.length - 2] + keyPress.keyCode + numbers[numbers.length - 1]);
+//         //     console.log(operate);
+//         //     console.log(typeof(operate));
+//         //     console.log(numberKey);
+//         // }
+//         if (keyName === "Enter"){
+//             numbers = [0];
+//             results = [];
+//         }
+//     }
+
+//     operation(Number(keyName));
+
+//     // if(keyName === "Backspace"){
+//     //     saveAll.pop();
+//     //     count --;
+//     //     calculations.innerText -= saveAll[saveAll.length - 1];
+//     //     // saveAll.forEach((value) => {
+
+//     //     // })
+//     //     return console.log(calculations.innerText, saveAll, count);
+//     // } else {
+//     //     saveAll.push(keyName);
+//     //     count ++
+//     //     calculations.innerText += saveAll[saveAll.length - 1];
+//     //     // saveAll.forEach((value) => {
+
+//     //     // })
+//     //     return console.log(calculations.innerText, saveAll, count);
+//     // }
+
+
+//     // console.log(keyPress)
+// }
+// const keys = document.addEventListener("keydown", getKey)
